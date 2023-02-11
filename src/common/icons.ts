fefc1fcd6d8f9e47ed7c7921d17193f55af89a63
Acquite <acquitefx@gmail.com>
2023-02-11 15:55:18 +0000

    port icon constant to vd :husk:

--- src/common/icons.ts
+++ src/common/icons.ts
@@ -1,26 +1,26 @@
 /**
- * @param getIDByName: Fetches the ID of an icon based on its name.
+ * @param getAssetIDByName: Fetches the ID of an icon based on its name.
  */
-import { getIDByName } from 'enmity/api/assets';
+import { getAssetIDByName } from "@vendetta/ui/assets";
 
 /** 
  * Icons used throughout PronounDB, available in a single place for ease of use.
  * @param {object} Icons: Object of Icons which are grouped by their place of use.
  */
 export default {
-    Failed: getIDByName('Small'),
-    Delete:  getIDByName('ic_message_delete'),
-    Copy:  getIDByName('toast_copy_link'),
-    Open:  getIDByName('ic_leave_stage'),
-    Clipboard:  getIDByName('pending-alert'),
-    Clock: getIDByName('clock'),
+    Failed: getAssetIDByName('Small'),
+    Delete:  getAssetIDByName('ic_message_delete'),
+    Copy:  getAssetIDByName('toast_copy_link'),
+    Open:  getAssetIDByName('ic_leave_stage'),
+    Clipboard:  getAssetIDByName('pending-alert'),
+    Clock: getAssetIDByName('clock'),
     Settings: {
         Toasts: {
-            Settings: getIDByName('ic_selection_checked_24px'),
-            Failed: getIDByName('ic_close_circle_24px')
+            Settings: getAssetIDByName('ic_selection_checked_24px'),
+            Failed: getAssetIDByName('ic_close_circle_24px')
         },
-        Initial: getIDByName('coffee'),
-        Update: getIDByName("discover"),
-        Locale: getIDByName("ic_locale_24px"),
+        Initial: getAssetIDByName('coffee'),
+        Update: getAssetIDByName("discover"),
+        Locale: getAssetIDByName("ic_locale_24px"),
     }
 };
